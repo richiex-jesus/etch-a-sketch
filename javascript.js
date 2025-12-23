a63b19bc01317eea50bda47a5827413d924f0bed
richiex-jesus <matiasrichard300@gmail.com>
2025-12-23 14:47:36 +0800

    completed UI for etch-a-sketch

--- javascript.js
+++ javascript.js
@@ -1,14 +1,49 @@
 let gridContainer = document.getElementById('grid-container');
 
 for (i = 0; i < 256; i++) {
-    console.log(i);
     let individualGrid = document.createElement('div');
     individualGrid.classList.add('gridDivs');
     individualGrid.setAttribute('style', 'display: flex; border: 1px solid black; height: 60px; width: 60px; box-sizing: border-box');
     gridContainer.appendChild(individualGrid);
 }
 
-let gridDiv = document.querySelector('.gridDivs')
+let gridDivs = document.querySelectorAll(".gridDivs");
 
+gridDivs.forEach(div => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'green';
+    })
+})
 
+let chooseButton = document.getElementById('new-grid');
+chooseButton.addEventListener('click', (e) => {
+    let input = prompt("Please put a number between 1 and 100.");
+    if (input > 100) {
+        alert("ERROR!");
+    } else {
+    gridContainer.replaceChildren();
+    createGrid(input);
+    newGridInteraction();
+    }
+})
 
+function createGrid(input) {
+    for (i = 0; i < input ** 2; i++) {
+        let individualGrid = document.createElement('div');
+        individualGrid.classList.add('gridDivs');
+        individualGrid.setAttribute('style', 'display: flex; border: 1px solid black; box-sizing: border-box');
+        let length = 960 / input;
+        individualGrid.style.height = `${length}px`;
+        individualGrid.style.width = `${length}px`;
+        gridContainer.appendChild(individualGrid)
+    }
+}
+
+function newGridInteraction() {
+    let gridDivs = document.querySelectorAll(".gridDivs")
+    gridDivs.forEach(div => {
+    div.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'green';
+    })
+    });
+}
