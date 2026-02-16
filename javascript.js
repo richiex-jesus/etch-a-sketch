7e0258a9fe579375c0f0bf9e256185883ce66734
richiex-jesus <matiasrichard300@gmail.com>
2026-02-16 15:05:41 +0800

    update grid limit 300

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ let gridContainer = document.getElementById('grid-container');
 for (i = 0; i < 256; i++) {
     let individualGrid = document.createElement('div');
     individualGrid.classList.add('gridDivs');
-    individualGrid.setAttribute('style', 'display: flex; border: 1px solid black; height: 60px; width: 60px; box-sizing: border-box');
+    individualGrid.setAttribute('style', 'display: flex; border: 0.5px solid lightgrey; height: 60px; width: 60px; box-sizing: border-box');
     gridContainer.appendChild(individualGrid);
 }
 
@@ -11,14 +11,14 @@ let gridDivs = document.querySelectorAll(".gridDivs");
 
 gridDivs.forEach(div => {
     div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'green';
+        e.target.style.backgroundColor = 'red';
     })
 })
 
 let chooseButton = document.getElementById('new-grid');
 chooseButton.addEventListener('click', (e) => {
-    let input = prompt("Please put a number between 1 and 100.");
-    if (input > 100) {
+    let input = prompt("Please put a number between 1 and 300.");
+    if (input > 300) {
         alert("ERROR!");
     } else {
     gridContainer.replaceChildren();
@@ -43,7 +43,7 @@ function createGrid(input) {
     for (i = 0; i < input ** 2; i++) {
         let individualGrid = document.createElement('div');
         individualGrid.classList.add('gridDivs');
-        individualGrid.setAttribute('style', 'display: flex; border: 1px solid black; box-sizing: border-box');
+        individualGrid.setAttribute('style', 'display: flex; border: 0.5px solid lightgrey; box-sizing: border-box');
         let length = 960 / input;
         individualGrid.style.height = `${length}px`;
         individualGrid.style.width = `${length}px`;
@@ -55,7 +55,7 @@ function newGridInteraction() {
     let gridDivs = document.querySelectorAll(".gridDivs");  
     gridDivs.forEach(div => {
     div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'green';
+        e.target.style.backgroundColor = 'red';
     })
     });
 }
